826cf17a9a62e76844291a661f0ca18b6725c55e
GowthamN <gn86923@gmail.com>
2025-09-14 13:00:33 +0530

    Feat: Central durableLogger (core implementatioms) added

---
+++ src/main/java/com/yourorg/logging/api/LogManager.java
@@ -0,0 +1,23 @@
+package com.yourorg.logging.api;
+
+import com.yourorg.logging.storage.StorageAdapter;
+
+public final class LogManager{
+    private static volatile LogManager INSTANCE;
+    private final StorageAdapter adapter;
+    private final com.yourorg.logging.core.DurableLogger logger;
+
+    private LogManager(StorageAdapter adapter,com.yourorg.logging.core.DurableLogger logger){
+        this.adpter=adapter; this.logger=logger;
+    }
+    public static synchronized void init(StorageAdapter adapter,com.yourorg.logging.core.DurableLogger logger){
+        INSTANCE = new LogManager(adapter,logger);
+    }
+    public static LogManager get(){return  INSTANCE;}
+
+    public Logger getLogger(Class<?> cls){
+        return logger.forClass(cls);
+    }
+    public com.yourorg.logging.core.DurableLogger getCorelogger(){return logger;}
+
+}
