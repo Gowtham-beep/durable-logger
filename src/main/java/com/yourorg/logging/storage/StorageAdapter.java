88725975651ffb693afe321ce97106be5eae6f0a
GowthamN <gn86923@gmail.com>
2025-09-14 13:08:47 +0530

    Feat: Minimal storageAdapter added

---
+++ src/main/java/com/yourorg/logging/storage/StorageAdapter.java
@@ -0,0 +1,20 @@
+package com.yourorg.logging.storage;
+
+import com.yourorg.logging.api.LogEntry;
+import com.yourorg.logging.core.QueryRequest;
+import com.yourorg.logging.core.Queryresult;
+
+import java.util.List;
+
+public interface StorageAdapter {
+    void start() throws Exception;
+    void stop() throws Exception;
+    /**
+     * Append a batch of entries to the underlying store. Implementations should
+     * be idempotent with respect to LogEntry.uuid.
+     */
+    void append(List<LogEntry> entries) throws Exception; /**
+     * Optional: query. Throw UnsupportedOperationException if not supported.
+     */
+    QueryResult query(QueryRequest request) throws UnsupportedOperationException;
+}
